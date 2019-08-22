0b9a590f16e3ba814ca9bb17058b77f318c3b46b
Peixuan <bphsuan@gmail.com>
2019-08-22 23:25:38 +0800

    cover src

---
+++ src/src/pages/Products.js
@@ -0,0 +1,9 @@
+import React from "react"
+import { Link } from 'gatsby'
+import Layout from '../components/layout/layout'
+import Products from '../components/Products/Products'
+export default () =>
+    <Layout>
+        <Products></Products>
+    </Layout>
+    
