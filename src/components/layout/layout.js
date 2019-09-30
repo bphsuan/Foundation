90eb2a424912d40c8ff5a413a298b35918d1c10e
jingyi <amychen861110@gmail.com>
2019-09-30 13:04:17 +0800

    connect register&login API success, per form format check

--- src/components/layout/layout.js
+++ src/components/layout/layout.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Provider } from 'react-redux';
+import { Helmet } from 'react-helmet';
 import { create } from 'dva-core';
 import memberModel from '../../models/memberModel';
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
 import ToTop from '../ToTop/ToTop';
 import './Layout.scss';
+import Favicon from '../../images/favicon.ico';
 
 const app = create();
 app.model(memberModel);
@@ -14,6 +16,11 @@ const store = app._store;
 
 export default ({ children }) => (
   <Provider store={store}>
+    <Helmet>
+      <meta charSet="utf-8" />
+      <title>紅粉知己 Find Your Foundation</title>
+      <link rel="icon" href={Favicon} type="image/x-icon" />
+    </Helmet>
     <div>
       <Header />
       <div className="content">
--- src/models/memberModel.js
+++ src/models/memberModel.js
@@ -1,17 +1,50 @@
 import { register, login } from '../services/memberService';
 
 export default {
-  namespace: 'member',
+  namespace: "member",
   state: {
-
+    memberAccount: "",
+    isLogin: false
   },
   reducers: {
-
+    SET_Login(state, { payload }) {
+      console.log(payload);
+      return {
+        ...state, //open state
+        memberAccount: payload,
+        isLogin: true
+      }
+    },
+    SET_Logout(state) {
+      return {
+        ...state,
+        isLogin: false
+      }
+    }
   },
   effects: {
     * register({ payload, callback }, { put, call, select }) {
-      yield call(register, payload);
-      callback()
+      const resMsg = yield call(register, payload); //return status
+      console.log(resMsg);
+      callback(resMsg)
+    },
+    * login({ payload, callback }, { put, call, select }) {
+      const resMsg = yield call(login, payload); //return status
+      console.log(resMsg);
+      let _account = "";
+      _account = payload.Account; //get account
+      console.log(_account);
+      yield put({
+        //to reducer
+        type: "SET_Login",
+        payload: _account,
+      });
+      callback(resMsg);
+    },
+    * logout({ payload, callback }, { put, call, select }) {
+      yield put({
+        type: "SET_Logout",
+      })
     }
   }
 }
