6d6f195b923d45b9666a19b287a9e2c2d7de0b5d
jychen <amychen861110@gmail.com>
2019-10-08 16:02:47 +0800

    fetch cookie fail

---
+++ src/pages/ProductDetail.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+import { Link } from 'gatsby';
+import Layout from '../components/Layout/Layout';
+import ProductDetail from '../components/ProductDetail/ProductDetail';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+class Detail extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    }
+  }
+  render() {
+    return (
+      <Layout>
+        <ProductDetail />
+      </Layout>
+    )
+  }
+}
+
+export default Detail;
