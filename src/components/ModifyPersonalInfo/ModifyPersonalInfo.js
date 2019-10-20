69a0db14416bc1ef4608c7418a1ca693490c6a75
whattiffany <1102b004@gmail.com>
2019-10-20 18:34:35 +0800

    個人資料修改完成(POST)

--- src/components/ModifyPersonalInfo/ModifyPersonalInfo.js
+++ src/components/ModifyPersonalInfo/ModifyPersonalInfo.js
@@ -8,7 +8,12 @@ class ModifyPersonalInfo extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      user: [],
+      Name: "",
+      Gender: "",
+      Birthday: "",
+      Email: "",
+      Phone: "",
+      Address: ""
     }
   }
   componentDidMount() {
@@ -19,23 +24,32 @@ class ModifyPersonalInfo extends React.Component {
     this.props.dispatch({
       type: "member/GET_userInfo",
       callback: response => {
-        console.log(response);
+        // console.log(response);
         this.setState({
-          user: response,
+          Name: response.Name,
+          Gender: response.Gender,
+          Birthday: response.Birthday,
+          Email: response.Email,
+          Phone: response.Phone,
+          Address: response.Address
         })
-        console.log(this.state.user);
+        //取年月日
+        let dateTemp = this.state.Birthday.split("T", 1)[0]
+        this.setState({
+          Birthday: dateTemp
+        })
+        // console.log(this.state);
       }
     })
   }
   Send_userInfo = () => {
     const userInfo = {
-      Name: this.state.user.name,
-      Gender: this.state.user.gender,
-      Birthday: this.state.user.birthday,
-      Email: this.state.user.email,
-      Phone: this.state.user.phone,
-      Address: this.state.user.address
-
+      Name: this.state.Name,
+      Gender: this.state.Gender,
+      Birthday: this.state.Birthday,
+      Email: this.state.Email,
+      Phone: this.state.Phone,
+      Address: this.state.Address
     }
     console.log(userInfo)
     this.props.dispatch({
@@ -51,42 +65,75 @@ class ModifyPersonalInfo extends React.Component {
       }
     })
   }
-  changeName = (e) => {
-    console.log(e.target)
-    this.setState({
-      // user: [, , e.target.value,],
-    })
-
+  changeItem = (e) => {
+    switch (e.target.id) {
+      case "Name":
+        this.setState({
+          Name: e.target.value
+        })
+        console.log("改名字!" + e.target.value);
+        break;
+      case "Gender":
+        this.setState({
+          Gender: e.target.value
+        })
+        console.log("改性別!" + e.target.value);
+        break;
+      case "Birthday":
+        this.setState({
+          Birthday: e.target.value
+        })
+        console.log("改生日!" + e.target.value);
+        break;
+      case "Email":
+        this.setState({
+          Email: e.target.value
+        })
+        console.log("改信箱!" + e.target.value);
+        break;
+      case "Phone":
+        this.setState({
+          Phone: e.target.value
+        })
+        console.log("改電話!" + e.target.value);
+        break;
+      case "Address":
+        this.setState({
+          Address: e.target.value
+        })
+        console.log("改地址!" + e.target.value);
+        break;
+    }
   }
   render() {
     return (
       <div className="modify-form" >
         <div className="input-style">
           <p className="letter">姓名</p>
-          <input type="text" className="input" defaultValue={this.state.user.Name} onChange={this.changeName} />
+          <input type="text" id="Name" className="input" defaultValue={this.state.Name} onChange={this.changeItem.bind(this)} />
         </div>
         <div className="input-style">
           <p className="letter">性別</p>
-          <select className="input" defaultValue={this.state.user.Gender}>
+          <select id="Gender" className="input" defaultValue={this.state.Gender} onChange={this.changeItem.bind(this)} >
             <option>女</option>
             <option>男</option>
           </select>
         </div>
         <div className="input-style">
           <p className="letter">生日</p>
-          <input type="date" className="input" defaultValue={this.state.user.Birthday} />
+          <input type="date" id="Birthday" className="input" defaultValue={this.state.Birthday} onChange={this.changeItem.bind(this)} />
         </div>
         <div className="input-style">
           <p className="letter">email</p>
-          <input type="email" className="input" defaultValue={this.state.user.Email} />
+          <input type="email" id="Email" className="input" defaultValue={this.state.Email} onChange={this.changeItem.bind(this)} />
         </div>
         <div className="input-style">
           <p className="letter">電話</p>
-          <input type="tel" className="input" defaultValue={this.state.user.Phone} />
+          <input type="tel" id="Phone" className="input" defaultValue={this.state.Phone} onChange={this.changeItem.bind(this)} />
         </div>
         <div className="input-style">
           <p className="letter">地址</p>
-          <input type="text" className="input" defaultValue={this.state.user.Address} />
+          <input type="text" id="Address" className="input" defaultValue={this.state.Address} onChange={this.changeItem.bind(this)} />
         </div>
         <div className="modify-btn">
           <a onClick={this.Send_userInfo.bind(this)}>儲存</a>
