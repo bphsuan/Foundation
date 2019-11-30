265f424527993bd48a7712623f56981e63ad9676
whattiffany <1102b004@gmail.com>
2019-11-30 23:40:11 +0800

    檢測結果(未完成)

--- src/services/faceService.js
+++ src/services/faceService.js
@@ -1,22 +1,26 @@
 const faceServer = "http://foundation.hsc.nutc.edu.tw/api/Face/";
 
-function uploadDetectionPic(Img) {
+async function uploadDetectionPic(Img) {
   console.log("進入service");
+
   const token = (localStorage.getItem("token")) ? JSON.parse(localStorage.getItem("token")) : {
     token: []
   };
-  return fetch(faceServer + "SkinDetection", {
+  const response = await fetch(faceServer + "SkinDetection", {
     method: "POST",
     headers: {
       //上傳圖片不需要先填content-type
       // "Content-Type": "application/x-www-form-urlencoded",
+      "Accept": "application/json",
       "Authorization": "Bearer " + token.token[0]
     },
     body: Img
-  }).then(response => {
-    console.log(response);
-    response.json()
-  })
+  });
+  // .then(response => {
+
+  // })
+  const json = response.json();
+  return json;
 }
 function getDetectHistory() {
   const token = (localStorage.getItem("token")) ? JSON.parse(localStorage.getItem("token")) : {
