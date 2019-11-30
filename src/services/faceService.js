c7ffccca138405a82174d8f1a85eee5f24c4a756
Peixuan <bphsuan@gmail.com>
2019-12-01 00:16:56 +0800

    detection response fix

--- src/services/faceService.js
+++ src/services/faceService.js
@@ -1,38 +1,35 @@
-const faceServer = "http://foundation.hsc.nutc.edu.tw/api/Face/";
+const faceServer = "http://foundation.hsc.nutc.edu.tw/api/Face/"
 
-async function uploadDetectionPic(Img) {
-  console.log("進入service");
-
-  const token = (localStorage.getItem("token")) ? JSON.parse(localStorage.getItem("token")) : {
-    token: []
-  };
-  const response = await fetch(faceServer + "SkinDetection", {
+function uploadDetectionPic(Img) {
+  const token = localStorage.getItem("token")
+    ? JSON.parse(localStorage.getItem("token"))
+    : {
+        token: [],
+      }
+  return fetch(faceServer + "SkinDetection", {
     method: "POST",
     headers: {
       //上傳圖片不需要先填content-type
       // "Content-Type": "application/x-www-form-urlencoded",
-      "Accept": "application/json",
-      "Authorization": "Bearer " + token.token[0]
+      Accept: "application/json",
+      Authorization: "Bearer " + token.token[0],
     },
-    body: Img
-  });
-  // .then(response => {
-
-  // })
-  const json = response.json();
-  return json;
+    body: Img,
+  }).then(response => response.json())
 }
 function getDetectHistory() {
-  const token = (localStorage.getItem("token")) ? JSON.parse(localStorage.getItem("token")) : {
-    token: []
-  };
+  const token = localStorage.getItem("token")
+    ? JSON.parse(localStorage.getItem("token"))
+    : {
+        token: [],
+      }
   return fetch(faceServer + "GetFaceHistoryByAcc", {
     method: "GET",
     headers: {
       "Content-Type": "application/json",
       "Access-Control-Allow-Origin": "*",
-      "Accept": "application/json",
-      "Authorization": "Bearer " + token.token[0]
+      Accept: "application/json",
+      Authorization: "Bearer " + token.token[0],
     },
   }).then(response => response.json())
 }
